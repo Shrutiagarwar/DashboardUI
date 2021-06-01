a707ebbbb3c0a6a0488d5575722863a1e4773304
Chikuai <chikuai54@gmail.com>
2021-06-01 20:00:43 +0530

    Random

---
+++ jj.js
@@ -0,0 +1,11 @@
+ for(let a=0;a<5;a++)
+        {
+            for( let b=0;b<5;b++)
+            {
+                
+              console.log("*")  
+                
+          }
+          console.log("\n")
+        
+    }
